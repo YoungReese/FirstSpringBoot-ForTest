d567dcb7ddebf97aea30dc50af5d4ee273d9ac8d
liyang <liyang.hlw@vivo.com>
2020-10-23 19:54:21 +0800

    first springboot project for test - helloworld

---
+++ helloworld/src/main/java/com/ly/helloworld/HelloworldApplication.java
@@ -0,0 +1,20 @@
+package com.ly.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// 本身就是Spring的一个组件
+
+// 程序主入口
+@SpringBootApplication
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+	void test() {
+		System.out.println("hello world!");
+	}
+}
